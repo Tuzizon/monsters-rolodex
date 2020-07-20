9ecee16f115dbe5a99776e5635c851c1522bf489
Arthur Bastos <tuzaobastos@gmail.com>
2020-07-20 00:03:07 -0300

    Updates

---
+++ precache-manifest.c43f345f3197eefadc518a94d190e177.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c415099351f301bfed65db0ea0ea04",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e47c8f042ba7fbf6399f",
+    "url": "/monsters-rolodex/static/css/main.fe3337de.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e47c8f042ba7fbf6399f",
+    "url": "/monsters-rolodex/static/js/main.4b8b0ccd.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
